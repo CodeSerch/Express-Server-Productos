d325d968c60d255109b8a5b8e07d1a228d21558f
CodeSerch <sergioenriquebg28@gmail.com>
2021-11-10 16:26:16 -0300

    commit

--- index.js
+++ index.js
@@ -3,11 +3,54 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const prueba1 = require('./programa');
 const path = require('path');
 const productos = new prueba1.Contenedor('./productos.txt')
 const app = express();
+const cors = require('cors');
+
+//socket io 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('new connection', socket.id);
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  })
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+
+/*io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+  });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+io.on('connection', (socket) => {
+  socket.broadcast.emit('hi');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});*/
+
+const messages = [
+  { author: "Juan", text: "¡Hola! ¿Que tal?" },
+  { author: "Pedro", text: "¡Muy bien! ¿Y vos?" },
+  { author: "Ana", text: "¡Genial!" }
+];
 
 //MySQL
 /*const connection = mysql.createConnection({
@@ -23,49 +66,46 @@ const app = express();
   console.log('Database server running');
 })
 */
+
 //definiendo objeto
-const objeto1 = {title:"producto1" ,price:2, url:"ejemplo.png"};
-const update1 = {title:"producto2" ,price:350, url:"ejemplo2.png"};
+const objeto1 = { title: "producto1", price: 2, url: "ejemplo.png" };
+const update1 = { title: "producto2", price: 350, url: "ejemplo2.png" };
 
 //app.listen(PORT, ()=> (console.log(`Server  running on port ${PORT}`)))
 
 //APIs
-const url="https://api.publicapis.org/entries";
+const url = "https://api.publicapis.org/entries";
 
 //Settings
 const PORT = 8080;
 
 //Middlewares
 //especificamos el subdirectorio donde se encuentran las páginas estáticas
-app.use(express.static(__dirname + '/public'));
-
-//extended: false significa que parsea solo string (no archivos de imagenes por ejemplo)
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(cors())
 
 //handlebars
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
-/*app.set('view engine', 'pug');
-
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
-});*/
-
-
 //Routes
-/*app.get('/', function(req, res) {
-  res.send('Welcome to my api');
+
+/*app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });*/
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
 
 app.get("/productos", async (req, res) => {
   const prods = await productos.getAll();
   console.log(prods);
-  res.render('view', {layout: 'main', title : JSON.stringify(prods) });
+  res.render('view', { layout: 'main', title: JSON.stringify(prods) });
+})
+
+app.get("/chat", (req, res) => {
+  res.render('chat', { layout: 'main', title: "chat" });
 })
 
 app.get("/productos/:id", async (req, res) => {
@@ -90,34 +130,34 @@ app.post('/add', async function (req, res) {
 });
 
 app.post('/addForm', async function (req, res) {
-  const objetoGuardar = {title:req.body.title ,price:req.body.price, url:req.body.imgUrl};
+  const objetoGuardar = { title: req.body.title, price: req.body.price, url: req.body.imgUrl };
   const prods = await productos.save(objetoGuardar);
   console.log(prods);
   res.redirect('/productos')
 });
 
-app.put('/update/:id', async (req, res) =>{
+app.put('/update/:id', async (req, res) => {
   const id = parseInt(req.params.id);
   //const {title, price, url} = req.body; objetoUpdate1
-  const prods = await productos.update(update1.title,update1.price,update1.url,id);
+  const prods = await productos.update(update1.title, update1.price, update1.url, id);
   console.log(prods);
   res.send("se actualizo el objeto con id " + prods)
 })
 
-app.delete('/delete/:id', async (req, res) =>{
+app.delete('/delete/:id', async (req, res) => {
   const id = parseInt(req.params.id);
   console.log("objeto a eliminar, id: " + id);
   await productos.deleteById(id);
   res.send("objeto con id: " + id + " eliminado");
 })
 
-app.delete('/deleteAll', async (req, res) =>{
+app.delete('/deleteAll', async (req, res) => {
   const respuesta = await productos.deleteAll();
   res.send(respuesta);
 })
-  
+
 //Start the server
-app.listen(PORT, function(err){
-    if (err) console.log(err);
-    console.log('Server started at http://localhost:' + PORT);
+server.listen(PORT, function (err) {
+  if (err) console.log(err);
+  console.log('Server started at http://localhost:' + PORT);
 });
